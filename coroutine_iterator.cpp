95680a9e8ce809af7b267890d21b2a821abee381
jemand2001 <>
2022-01-01 20:24:14 +0100

    tests + starting on `yield from`

---
+++ coroutine_iterator.cpp
@@ -0,0 +1,56 @@
+#include <coroutine>
+#include <iostream>
+#include <stdexcept>
+#include "generator.hpp"
+#include "yield_from.hpp"
+
+using namespace base;
+
+generator<int> void_iterator() {
+    // std::cout << "beginning\n";
+    co_yield 1;
+    // std::cout << "after 1st yield\n";
+    co_yield 2;
+    // std::cout << "after 2nd yield\n";
+    co_yield 3;
+    // std::cout << "after 3rd yield\n";
+    co_return;
+}
+
+generator<int, int> non_void_iterator() {
+    // std::cout << "beginning\n";
+    co_yield 1;
+    // std::cout << "after 1st yield\n";
+    co_yield 2;
+    // std::cout << "after 2nd yield\n";
+    co_yield 3;
+    // std::cout << "after 3rd yield\n";
+    co_return 12;
+}
+
+generator<int> yield_from_test() {
+    co_await yield_from{void_iterator()};
+    co_return;
+}
+
+int main() {
+    for (int i : void_iterator()) {
+        std::cout << i << '\n';
+    }
+
+    std::cout << '\n';
+
+    auto g = non_void_iterator();
+    for (int i : g) {
+        std::cout << i << '\n';
+    }
+    std::cout << "return value: " << g.return_value() << '\n';
+
+    std::cout << '\n';
+
+    for (int i : yield_from_test()) {
+        std::cout << i << '\n';
+    }
+
+    return 0;
+}
---
+++ yield_from.hpp
@@ -0,0 +1,37 @@
+#include <coroutine>
+#include "generator.hpp"
+
+#include <iostream>
+#include <ranges>
+#include <iterator>
+
+template <typename Range>
+struct yield_from {
+    using generator =
+        base::generator<std::remove_reference_t<typename std::iterator_traits<typename Range::iterator>::value_type>,
+                        void>;
+    using _generator_promise = generator::promise_type;
+
+    using _iterator = Range::iterator;
+
+    _iterator begin;
+    _iterator end;
+
+    std::coroutine_handle<_generator_promise> dst_handle;
+
+    bool await_ready() { return begin != end; }
+
+    void await_suspend(auto h) {
+        dst_handle = h;
+        for (auto it = begin; it != end; it++) {
+            std::cout << "what\n";
+            dst_handle.promise().yield_value(*it);
+        }
+    }
+
+    void await_resume() {}
+
+    yield_from(yield_from<Range> &) = default;
+    yield_from(Range &rng) : begin(rng.begin()), end(rng.end()) {}
+    yield_from(Range &&rng) : begin(rng.begin()), end(rng.end()) {}
+};
