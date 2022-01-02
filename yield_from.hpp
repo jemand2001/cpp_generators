829998f08759cb3ffc4ff0f7d3b2ed5861d20a50
jemand2001 <>
2022-01-02 12:59:48 +0100

    made yield_from more generic

--- yield_from.hpp
+++ yield_from.hpp
@@ -10,7 +10,7 @@
 /**
  * @brief yield the elements of a range in order
  * this should behave similarly to python's `yield from`
- * 
+ *
  * usage: `co_await yield_from(my_range);`
  * @tparam Range the type of the range to yield from
  */
@@ -18,14 +18,13 @@ template <typename Range>
 struct yield_from {
     using value_type = std::remove_reference_t<
         typename std::iterator_traits<typename Range::iterator>::value_type>;
-    using generator = base::generator<value_type, void>;
-    using _generator_promise = generator::promise_type;
 
     Range &rng;
 
     bool await_ready() { return rng.begin() == rng.end(); }
 
-    bool await_suspend(std::coroutine_handle<_generator_promise> h) {
+    template <typename Promise>
+    bool await_suspend(std::coroutine_handle<Promise> h) {
         for (auto i : rng) {
             h.promise().yield_value(i);
         }
